7d4a4cb84eda6e069bd95f4caa6ad314a9f06a28
KaranMohindroo <karanmohindroo14@gmail.com>
2018-12-15 22:56:02 +0530

    Update 1.cpp

--- Solutions/1.cpp
+++ Solutions/1.cpp
@@ -1,1 +1,16 @@
-
+int check_K_Sum_Pair(int *arr,int size,int k)
+{
+	int hash[100000]={0},i;
+	for(i=0;i<size;i++)
+	{
+		hash[arr[i]]++;
+	}
+	for(i=0;i<size;i++)
+	{
+		if(hash[k-arr[i]]>0)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}

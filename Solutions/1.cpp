a13956adb06a6cd5f1d8fce482495475041a1a8f
KaranMohindroo <karanmohindroo14@gmail.com>
2018-12-15 22:57:39 +0530

    Update 1.cpp

--- Solutions/1.cpp
+++ Solutions/1.cpp
@@ -3,11 +3,11 @@ int check_K_Sum_Pair(int *arr,int size,int k)
 	int hash[100000]={0},i;
 	for(i=0;i<size;i++)
 	{
-		hash[arr[i]]++;
+		hash[arr[i]]++;				//Hashing
 	}
 	for(i=0;i<size;i++)
 	{
-		if(hash[k-arr[i]]>0)
+		if(hash[k-arr[i]]>0)		//check if k-current element exists in hash
 		{
 			return 1;
 		}
